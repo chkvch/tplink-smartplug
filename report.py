c36ba6d7b5ed14dbc76e22946033dcc91fc2336e
Chris Mankovich <cmankovich@ucsc.edu>
2019-01-08 15:53:51 -0800

    working report for ground floor

--- report.py
+++ report.py
@@ -3,13 +3,16 @@
 #%matplotlib inline
 import pickle
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import os
 
-with open('smartplug_192.168.1.113.pkl', 'rb') as f:
+with open('{}/smartplug_192.168.1.113.pkl'.format(os.environ['HOME']), 'rb') as f:
     log = pickle.load(f)
 
-print(list(log)[0])
-print(log[list(log)[0]])
+#print(list(log)[0])
+#print(log[list(log)[0]])
 
 history = {}
 for stamp in sorted(list(log)):
@@ -23,14 +26,14 @@ history['time'] = np.array(sorted(list(log)))
 #fig, ax = plt.subplots(4, 1, figsize=(15, 10), sharex=True, gridspec_kw={'hspace':0.1})
 fig, ax = plt.subplots(4, 1, figsize=(15, 10), sharex=True)
 
-kwargs = {'marker':'o', 'markersize':3}
+kwargs = {'linestyle':'-', 'lw':1, 'marker':'o', 'markersize':2}
 ax[0].plot(history['time'], history['current'], **kwargs)
-ax[1].plot(history['time'], history['voltage'])
-ax[2].plot(history['time'], history['power'])
-ax[3].plot(history['time'], history['total'] - history['total'][0])
+ax[1].plot(history['time'], history['voltage'], **kwargs)
+ax[2].plot(history['time'], history['power'], **kwargs)
+ax[3].plot(history['time'], history['total'] - history['total'][0], **kwargs)
 from scipy.integrate import cumtrapz
 int_p_dt = cumtrapz(history['power'], x=history['time'], initial=0.) / 3600. / 1e3
-ax[3].plot(history['time'], int_p_dt, '-.')
+ax[3].plot(history['time'], int_p_dt, **kwargs)
 
 ax[0].set_ylabel(r'Current (A)')
 ax[1].set_ylabel(r'Voltage (V)')
@@ -39,10 +42,15 @@ ax[3].set_ylabel(r'Energy (kWh)')
 ax[-1].set_xlabel(r'Time (Unix)')
 
 from datetime import datetime
-date = datetime.utcfromtimestamp(int(history['time'][0]) - 8 * 3600).strftime('%Y-%m-%d')
-time0 = datetime.utcfromtimestamp(int(history['time'][0]) - 8 * 3600).strftime('%H:%M:%S')
-time1 = datetime.utcfromtimestamp(int(history['time'][-1]) - 8 * 3600).strftime('%H:%M:%S')
+#date = datetime.utcfromtimestamp(int(history['time'][0]) - 8 * 3600).strftime('%Y-%m-%d')
+time0 = datetime.utcfromtimestamp(int(history['time'][0]) - 8 * 3600).strftime('%m/%d/%Y %H:%M:%S')
+time1 = datetime.utcfromtimestamp(int(history['time'][-1]) - 8 * 3600).strftime('%m/%d/%Y %H:%M:%S')
 
-ax[0].set_title('First floor furnace {}, {} to {}'.format(date, time0, time1))
-plt.savefig('history_test.pdf', bbox_inches='tight')
-plt.savefig('history_test.png', bbox_inches='tight')
+print('{} records from {} to {}'.format(len(history['time']), time0, time1))
+
+ax[0].set_title('First floor furnace: {} to {}'.format(time0, time1))
+#plt.savefig('history_test.pdf', bbox_inches='tight')
+plt.savefig('/home/pi/tplink-smartplug/history_test.png', bbox_inches='tight')
+
+os.system('sudo cp /home/pi/tplink-smartplug/history_test.png /var/www/html/power/history_test.png')
+os.system('sudo touch /var/www/html/power/index.html')
