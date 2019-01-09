c8c144a8f5b6eaab83fa7c2d189741727f4bc839
Christopher Mankovich <cmankovich@ucsc.edu>
2019-01-08 21:21:17 -0800

    reporter class; html root from environment variable

--- report.py
+++ report.py
@@ -8,61 +8,93 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import os
 from spcollector import logdbg
+from datetime import datetime
 
-for address in '192.168.1.112', '192.168.1.113':
+class reporter:
 
-    try:
+    def __init__(self, ip):
         inpath = '{}/smartplug_{}.pkl'.format(os.environ['HOME'], address)
         with open(inpath, 'rb') as f:
             log = pickle.load(f)
-    except Exception, e:
-        logdbg('failed to load database {}: {}'.format(inpath, e))
-        continue
 
-    history = {}
-    for stamp in sorted(list(log)):
-        for key in list(log[stamp]):
-            if not key in list(history):
-                history[key] = np.array([])
-            history[key] = np.append(history[key], float(log[stamp][key]))
+        self.history = {}
+        for stamp in sorted(list(log)):
+            for key in list(log[stamp]):
+                if not key in list(self.history):
+                    self.history[key] = np.array([])
+                self.history[key] = np.append(self.history[key], float(log[stamp][key]))
 
-    history['time'] = np.array(sorted(list(log)))
+        self.history['time'] = np.array(sorted(list(log)))
 
-    #fig, ax = plt.subplots(4, 1, figsize=(15, 10), sharex=True, gridspec_kw={'hspace':0.1})
-    fig, ax = plt.subplots(4, 1, figsize=(15, 10), sharex=True)
+        from scipy.integrate import cumtrapz
+        int_p_dt = cumtrapz(self.history['power'], x=self.history['time'], initial=0.) / 3600. / 1e3
+        self.history['integrated_power'] = int_p_dt
 
-    kwargs = {'linestyle':'-', 'lw':1, 'marker':'o', 'markersize':2}
-    ax[0].plot(history['time'], history['current'], **kwargs)
-    ax[1].plot(history['time'], history['voltage'], **kwargs)
-    ax[2].plot(history['time'], history['power'], **kwargs)
-    ax[3].plot(history['time'], history['total'] - history['total'][0], **kwargs)
-    from scipy.integrate import cumtrapz
-    int_p_dt = cumtrapz(history['power'], x=history['time'], initial=0.) / 3600. / 1e3
-    ax[3].plot(history['time'], int_p_dt, **kwargs)
+        self.label = {
+            'current':r'Current (A)',
+            'voltage':r'Voltage (V)',
+            'power':r'Power (W)',
+            'total':r'Energy (kWh)',
+            'time':r'Time (Unix)'
+        }
 
-    ax[0].set_ylabel(r'Current (A)')
-    ax[1].set_ylabel(r'Voltage (V)')
-    ax[2].set_ylabel(r'Power (W)')
-    ax[3].set_ylabel(r'Energy (kWh)')
-    ax[-1].set_xlabel(r'Time (Unix)')
+        assert 'POWER_HTML_ROOT' in list(os.environ), 'must set POWER_HTML_ROOT'
+        self.outdir = os.environ['POWER_HTML_ROOT']
+        self.ip = ip
 
-    from datetime import datetime
-    # minus 8 converts to PST
-    #date = datetime.utcfromtimestamp(int(history['time'][0]) - 8 * 3600).strftime('%Y-%m-%d')
-    time0 = datetime.utcfromtimestamp(int(history['time'][0]) - 8 * 3600).strftime('%m/%d/%Y %H:%M:%S')
-    time1 = datetime.utcfromtimestamp(int(history['time'][-1]) - 8 * 3600).strftime('%m/%d/%Y %H:%M:%S')
+    def alltime(self):
 
-    description = {'192.168.1.113':'First floor furnace', '192.168.1.112':'Attic furnace'}[address]
-    alias = {'192.168.1.113':'GroundFloor', '192.168.1.112':'Attic'}[address]
+        fig, ax = plt.subplots(4, 1, figsize=(15, 10), sharex=True)
 
-    info = '{}: {} records from {} to {}'.format(description, len(history['time']), time0, time1)
-    print(info)
-    logdbg(info)
-    ax[0].set_title(info)
-    #plt.savefig('history_test.pdf', bbox_inches='tight')
-    outfile = '/home/pi/tplink-smartplug/history_{}.png'.format(alias)
-    plt.savefig(outfile, bbox_inches='tight')
+        kwargs = {'linestyle':'-', 'lw':1, 'marker':'o', 'markersize':2}
+        ax[0].plot(self.history['time'], self.history['current'], **kwargs)
+        ax[1].plot(self.history['time'], self.history['voltage'], **kwargs)
+        ax[2].plot(self.history['time'], self.history['power'], **kwargs)
+        ax[3].plot(self.history['time'], self.history['total'] - self.history['total'][0], **kwargs)
+        ax[3].plot(self.history['time'], self.history['integrated_power'], **kwargs)
 
-    os.system('sudo cp {} /var/www/html/power/history_{}.png'.format(outfile, alias))
+        ax[0].set_ylabel(self.label['current'])
+        ax[1].set_ylabel(self.label['voltage'])
+        ax[2].set_ylabel(self.label['power'])
+        ax[3].set_ylabel(self.label['total'])
+        ax[-1].set_xlabel(self.label['time'])
 
-os.system('sudo touch /var/www/html/power/index.html')
+        # minus 8 converts to PST
+        #date = datetime.utcfromtimestamp(int(history['time'][0]) - 8 * 3600).strftime('%Y-%m-%d')
+        time0 = datetime.utcfromtimestamp(int(self.history['time'][0]) - 8 * 3600).strftime('%m/%d/%Y %H:%M:%S')
+        time1 = datetime.utcfromtimestamp(int(self.history['time'][-1]) - 8 * 3600).strftime('%m/%d/%Y %H:%M:%S')
+
+        try:
+            description = {'192.168.1.113':'First floor furnace', '192.168.1.112':'Attic furnace'}[self.ip]
+            alias = {'192.168.1.113':'GroundFloor', '192.168.1.112':'Attic'}[self.ip]
+        except KeyError:
+            description = alias = 'undefined'
+
+        info = '{}: {} records from {} to {}'.format(description, len(self.history['time']), time0, time1)
+        print(info)
+        logdbg(info)
+
+        ax[0].set_title(info)
+        #plt.savefig('history_test.pdf', bbox_inches='tight')
+        outfile = 'history_{}.png'.format(alias)
+        plt.savefig(outfile, bbox_inches='tight')
+
+        os.system('sudo cp {} {}/history_{}.png'.format(outfile, self.outdir, alias))
+        os.system('sudo touch {}/index.html'.format(self.outdir))
+
+    def daily(self):
+        pass
+
+    def weekly(self):
+        pass
+
+    def monthly(self):
+        pass
+
+    def yearly(self):
+        pass
+
+if __name__ == '__main__':
+    for address in '192.168.1.112', '192.168.1.113':
+        r = reporter(address)
+        r.alltime()
