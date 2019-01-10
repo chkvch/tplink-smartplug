c63e074a1e7c112c5340c18f72f9df228fc17e55
Christopher Mankovich <cmankovich@ucsc.edu>
2019-01-09 17:59:09 -0800

    correct header

--- to_csv.py
+++ to_csv.py
@@ -9,7 +9,7 @@ for ip in ('192.168.1.112', '192.168.1.113'):
 
     with open('{}/smartplug_{}.csv'.format(os.environ['HOME'], ip), 'wb') as f:
 
-        f.write('current,voltage,power,total\n')
+        f.write('unixtime,current,voltage,power,total\n')
         for stamp in sorted(list(log)):
             data = log[stamp]
             line = '{},{},{},{},{}\n'.format(stamp, data['current'], data['voltage'], data['power'], data['total'])
