fade6e452a268d8fb27d65ac644f3908071aceb5
Christopher Mankovich <cmankovich@ucsc.edu>
2019-01-09 17:55:06 -0800

    quick script to dump database as csv

---
+++ to_csv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import pickle
+import os
+for ip in ('192.168.1.112', '192.168.1.113'):
+
+    with open('{}/smartplug_{}.pkl'.format(os.environ['HOME'], ip), 'rb') as f:
+        log = pickle.load(f)
+
+    with open('{}/smartplug_{}.csv'.format(os.environ['HOME'], ip), 'wb') as f:
+
+        f.write('current,voltage,power,total\n')
+        for stamp in sorted(list(log)):
+            data = log[stamp]
+            line = '{},{},{},{}\n'.format(data['current'], data['voltage'], data['power'], data['total'])
+            f.write(line)
